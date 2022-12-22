e85b58231f9d93c4d3e726f9f92811e63418134e
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2022-12-22 23:11:45 +0300

    fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import csv
 
 
 # link = input('pls enter the link: ')
-link = 'https://www.yallakora.com/match-center/?date=12/22/2022'
+link = 'https://www.yallakora.com/match-center/?date=12/14/2022'
 
 html = requests.get(link)
 
